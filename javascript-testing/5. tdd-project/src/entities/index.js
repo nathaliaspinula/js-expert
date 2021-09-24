a389c7a8ee68b79e17f82bfdc3012a99e7245057
nathaliaspinula <nathaliaspinula@gmail.com>
2021-09-23 22:31:40 -0300

    feat: start renting a car project

---
+++ javascript-testing/5. tdd-project/src/entities/index.js
@@ -0,0 +1,9 @@
+const Car = require('./car');
+const CarCategory = require('./carCategory');
+const Customer = require('./customer');
+
+module.exports = {
+    Car,
+    CarCategory,
+    Customer
+}
