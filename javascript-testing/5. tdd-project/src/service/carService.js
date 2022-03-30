5176c565a0b0508cd8d8b91875000618757cd7ad
nathaliaspinula <nathaliaspinula@gmail.com>
2022-03-30 20:46:16 -0300

    feat: add rent function and unit test

--- javascript-testing/5. tdd-project/src/service/carService.js
+++ javascript-testing/5. tdd-project/src/service/carService.js
@@ -1,8 +1,15 @@
 const BaseRepository = require('../repository/base/baseRepository');
+const Tax = require('../entities/tax');
+const Transaction = require('../entities/transaction');
 
 class CarService {
     constructor({ cars }) {
-        this.carRepository = new BaseRepository({ file: cars })
+        this.carRepository = new BaseRepository({ file: cars });
+        this.taxesBasedOnAge = Tax.taxesBasedOnAge;
+        this.currencyFormat = new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        });
     };
 
     async getAvailableCar(carCategory) {
@@ -25,6 +32,46 @@ class CarService {
         const selectedCar = carCategory.carIds[randomCarIndex];
 
         return selectedCar;
+    };
+
+
+    calculateFinalPrice(customer, carCategory, numberOfDays) {
+        const { age } = customer;
+        const { price } = carCategory;
+        const { then: tax } = this.taxesBasedOnAge.find(
+            tax => age >= tax.from && age <= tax.to
+        );
+
+        const finalPrice = ((tax * price) * numberOfDays);
+
+        return this.currencyFormat.format(finalPrice);
+    };
+
+    async rent(customer, carCategory, numberOfDays) {
+        const car = await this.getAvailableCar(carCategory);
+
+        const finalPrice = await this.calculateFinalPrice(customer, carCategory, numberOfDays);
+
+        const today = new Date();
+
+        today.setDate(today.getDate() + numberOfDays);
+
+        const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        }
+
+        const dueDate = today.toLocaleDateString("pt-br", options);
+
+        const transaction = new Transaction({
+            customer,
+            car, 
+            dueDate,
+            amount: finalPrice
+        });
+
+        return transaction;
     }
 };
 
